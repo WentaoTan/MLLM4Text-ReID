85872d880af167dc87232276c284240020baee1b
TwT <33800731+WentaoTan@users.noreply.github.com>
2024-06-29 20:54:21 +0800

    Update run.sh
    An error pointed by DJEddyking. Thank you!

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python train.py \
 --name Pretrain \
 --img_aug \
---batch_size 256 \
+--batch_size 512 \
 --MLM \
 --dataset_name $DATASET_NAME \
 --loss_names 'sdm' \
